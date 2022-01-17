1914a47e2df7529a4f26e49da1d26d64aa16fd65
philgreening <p.greening@bham.ac.uk>
2022-01-17 15:38:51 +0000

    Created models and added first record

---
+++ persApp/persDb/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class PersdbConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'persDb'
---
+++ persApp/persDb/models.py
@@ -0,0 +1,21 @@
+from django.db import models
+
+class PeriodicalsOctavo(models.Model):
+    mms_id = models.TextField(null=False, blank=False)
+    issn = models.TextField(null=True, blank=True)
+    library_name = models.CharField(max_length=50, null=False, blank=False)
+    new_library = models.CharField(max_length=10, null=True, blank=True)
+    location_name = models.CharField(max_length=20, null=False, blank=False)
+    new_location = models.CharField(max_length=20, null=True, blank=True)
+    classmark = models.CharField(max_length=20, null=True, blank = True)
+    new_per_number = models.CharField(max_length=10, null=True, blank=True)
+    title = models.CharField(max_length=250, null = False, blank=False)
+    holdings_and_wants = models.CharField(max_length=500, null=False, blank=False)
+    new_holdings = models.CharField(max_length=500, null=True, blank=True)
+    new_wants_and_notes = models.CharField(max_length=500, null=True, blank=True)
+    labeled_on_shelf = models.BooleanField(default=False)
+    amended_on_alma = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.mms_id
+
