ac89477fa9a49f0c5d3738ce1b05914dd6976536
philgreening <philgreening@hotmail.com>
2022-02-10 17:00:27 +0000

    Added search bar and results page. Loaded bootsrap & fontawesome

--- persApp/persApp/settings.py
+++ persApp/persApp/settings.py
@@ -38,6 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'bootstrap5',
+    'fontawesomefree',
 ]
 
 MIDDLEWARE = [
@@ -55,7 +57,7 @@ ROOT_URLCONF = 'persApp.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [str(BASE_DIR.joinpath('templates'))],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- persApp/persDb/views.py
+++ persApp/persDb/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
+from django.views.generic import TemplateView, ListView
+from django.db.models import Q
+from .models import *
 
-# Create your views here.
+class HomePageView(TemplateView):
+    template_name = 'home.html'
+
+class SearchResultsView(ListView):
+    model = PeriodicalsOctavo
+    template_name = 'search_results.html'
+
+    def get_queryset(self):
+        query = self.request.GET.get('q')
+        object_list = PeriodicalsOctavo.objects.filter(
+            Q(title__icontains=query) | Q(mms_id__icontains=query) | Q(issn__icontains=query)
+        )
+        return object_list
+
+
+
+
