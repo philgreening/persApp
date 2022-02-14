1313f47ad5b6d22e6fa3094cb1dfa20a25f82e51
philgreening <philgreening@hotmail.com>
2022-02-14 16:46:17 +0000

    adding forms

--- persApp/persDb/urls.py
+++ persApp/persDb/urls.py
@@ -1,3 +1,4 @@
+from unicodedata import name
 from django.urls import path
 
 from .views import *
@@ -5,4 +6,5 @@ from .views import *
 urlpatterns = [
     path('search/', SearchResultsView.as_view(), name='search_results'),
     path('', HomePageView.as_view(), name='home'),
+    path('updated/<int:pk>', UpdatePerView.as_view(), name='update_per'),
 ]
--- persApp/persDb/views.py
+++ persApp/persDb/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
-from django.views.generic import TemplateView, ListView
+from django.views.generic import TemplateView, ListView, UpdateView
 from django.db.models import Q
+from .forms import *
+
 from .models import *
 
 class HomePageView(TemplateView):
@@ -10,13 +12,34 @@ class SearchResultsView(ListView):
     model = PeriodicalsOctavo
     template_name = 'search_results.html'
 
+    fields = [
+        'mms_id', 'issn', 'library_name',
+         'new_library', 'location_name',
+         'new_location', 'classmark', 'new_per_number',
+         'title', 'holdings_and_wants', 'new_holdings',
+         'new_wants_and_notes', 'labeled_on_shelf', 'amended_on_alma'
+    ]
+
     def get_queryset(self):
         query = self.request.GET.get('q')
         object_list = PeriodicalsOctavo.objects.filter(
             Q(title__icontains=query) | Q(mms_id__icontains=query) | Q(issn__icontains=query)
         )
         return object_list
+    
+    # def put(self, request, *args, **kwargs):
+    #     return self.update(request, *args, **kwargs)
 
 
 
+class UpdatePerView(UpdateView):
+    model = PeriodicalsOctavo
+    template_name = 'home.html'
 
+    fields = [
+        'mms_id', 'issn', 'library_name',
+         'new_library', 'location_name',
+         'new_location', 'classmark', 'new_per_number',
+         'title', 'holdings_and_wants', 'new_holdings',
+         'new_wants_and_notes', 'labeled_on_shelf', 'amended_on_alma'
+    ]
