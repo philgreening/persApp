743c38c8d0c6b9a3495bc04993888bc2a797d1e9
Phil Greening <philgreening@hotmail.com>
2022-04-07 09:54:08 +0100

    add heroku settings

--- persApp/persApp/settings.py
+++ persApp/persApp/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = 'django-insecure-^dxw5c5_6_+!axrhkh*2ws9^k36ja8kjd4-spt^tg!*15mihxp
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'https://periodical-database.herokuapp.com/']
 
 
 # Application definition
@@ -80,20 +80,22 @@ WSGI_APPLICATION = 'persApp.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-
 # DATABASES = {
-#         'default': {
-#             'ENGINE': 'django.db.backends.postgresql_psycopg2',
-#             'NAME': 'ciba',
-#         }
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
 #     }
+# }
 
+DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql_psycopg2',
+            'NAME': 'ciba',
+        }
+    }
+
+# db_from_env = dj_database_url.config(conn_max_age=600)
+# DATABASES['default'].update(db_from_env)
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
