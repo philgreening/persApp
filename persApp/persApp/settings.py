8665a422069f0dc0928f5bdba7c939e7e5c52cda
philgreening <philgreening@hotmail.com>
2022-05-09 15:37:29 +0100

    changed to production added .env settings

--- persApp/persApp/settings.py
+++ persApp/persApp/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 import os
+from dotenv import load_dotenv, find_dotenv
 
 import mimetypes
 mimetypes.add_type("text/css", ".css", True)
@@ -20,14 +21,22 @@ mimetypes.add_type("text/css", ".css", True)
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
+
+
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-^dxw5c5_6_+!axrhkh*2ws9^k36ja8kjd4-spt^tg!*15mihxp'
+# SECRET_KEY = 'django-insecure-^dxw5c5_6_+!axrhkh*2ws9^k36ja8kjd4-spt^tg!*15mihxp'
+
+dotenv_file = os.path.join(BASE_DIR, ".env")
+if os.path.isfile(dotenv_file):
+    load_dotenv(dotenv_file, encoding='utf8',)
+
+SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'philgreening.pythonanywhere.com']
 
